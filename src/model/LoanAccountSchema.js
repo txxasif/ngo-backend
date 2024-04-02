ead48ddee4ed92beb9cf3534a774e137c60aecfa
txxasif <txxasif@gmail.com>
2024-04-02 22:27:35 +0600

    loan

---
+++ src/model/LoanAccountSchema.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+// Transaction Schema
+const transactionSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+});
+const withdrawSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+});
+const depositAccountSchema = new mongoose.Schema(
+  {
+    memberId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "LocalUser",
+      required: true,
+    },
+    paymentTerm: {
+      type: String,
+      enum: [
+        "Daily",
+        "Weekly",
+        "Fortnightly",
+        "Monthly",
+        "Quarterly",
+        "Half-Yearly",
+        "Yearly",
+      ],
+      required: true,
+    },
+    periodOfTimeInMonths: {
+      type: Number,
+      required: true,
+    },
+    perInstallment: {
+      type: Number,
+      required: true,
+    },
+    profitPercentage: {
+      type: Number,
+      required: true,
+    },
+    onMatureAmount: {
+      type: Number,
+      required: true,
+    },
+    openingDate: {
+      type: Date,
+      required: true,
+    },
+    matureDate: {
+      type: Date,
+      required: true,
+    },
+    // firstDueDate: {
+    //   type: Date,
+    //   required: true,
+    // },
+    transactions: [transactionSchema],
+    withdraws: [withdrawSchema],
+    balance: {
+      type: Number,
+    },
+    isOpen: {
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const DepositAccount =
+  mongoose.models.DepositAccount ||
+  mongoose.model("DepositAccount", depositAccountSchema);
+const Transaction =
+  mongoose.models.Transaction ||
+  mongoose.model("Transaction", transactionSchema);
+const Withdraw =
+  mongoose.models.Withdraw || mongoose.model("Withdraw", withdrawSchema);
+module.exports = { DepositAccount, Withdraw, Transaction };
